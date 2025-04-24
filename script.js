435a5466a5bda83da8e39b9dd6dfcca5f561b061
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-24 19:26:49 +0800

    Add feature: delete book from library

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 const bookShelf = document.querySelector(".book-shelf");
 const modalContainer = document.querySelector(".modal-container");
+const modalForm = document.querySelector(".modal-form");
 const btnShowModal = document.getElementById("btn-show-modal");
 const btnSubmit = document.getElementById("btn-submit");
 const btnCancel = document.getElementById("btn-cancel");
@@ -22,26 +23,35 @@ function getBookInfo() {
     let newBook = new Book(title.value, author.value, pages.value, isRead.checked);
 
     addBookToLibrary(newBook);
-
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    isRead.checked = false;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function deleteBook(library, id) {
+    const bookIndex = library.findIndex((book) => book.id === id);
+    library.splice(bookIndex, 1);
+}
+
 function displayLibraryBooks() {
     bookShelf.innerHTML = myLibrary.map((book) => 
-        `<div class="book-card">
+        `<div class="book-card" data-id="${book.id}">
             <p>${book.title}</p>
             <p>${book.author}</p>
             <p>${book.pages}</p>
-            <p>${book.isRead}</p>
+            <p>${book.isRead ? `Read`:`Not Read`}</p>
+            <button class="btn-delete" type="button">Delete</button>
         </div>`
     ).join("")
+
+    const btnDelete = document.querySelectorAll(".btn-delete");
+    btnDelete.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            deleteBook(myLibrary, btn.id);
+            displayLibraryBooks();
+        });
+    });
 }
 
 btnShowModal.addEventListener("click", () => {
@@ -57,6 +67,7 @@ btnSubmit.addEventListener("click", (e) => {
     e.preventDefault();
     getBookInfo();
     displayLibraryBooks();
+    modalForm.reset(); 
     modalContainer.close();
 });
 
